e1721501a4b1589a4b9f0c9ed8ef3c2b10226353
erey21 <arinerey@yandex.ru>
2021-11-04 21:24:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int num_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int num_2 = Convert.ToInt32(Console.ReadLine());
+            if (num_1 > num_2)
+            {
+                Console.WriteLine("Первое число больше второго.");
+            }
+            else if (num_1 < num_2)
+            {
+                Console.WriteLine("Второе число больше первого.");
+            }
+            else {
+                Console.WriteLine("Два числа равны.");
+            }
+        }
+    }
+}
